87f7b15e827c627aeb51a0e151f9dff2e295ffe0
Rafa-3000 <2024321893@aluno.canoas.ifrs.edu.br>
2026-08-27 18:01:06 -0300

    Cria superclasse Publicacao

---
+++ atv_avaliativa1_2tri/Publicacao.java
@@ -0,0 +1,14 @@
+package atv_avaliativa1_2tri;
+
+public class Publicacao{
+
+    private String titulo;
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public Publicacao(String titulo){
+        this.titulo=titulo;
+    }
+}
